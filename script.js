e759c98f5f934ca54fa62189768ca9c6c525154d
unknown <cmoreno@codingdojo.la>
2024-08-30 00:59:17 -0400

    archivos sin corregir responsive

---
+++ script.js
@@ -0,0 +1,22 @@
+function initMap() {
+    const map= new google.maps.Map(document.getElementById("map"),{
+        center: { lat: -33.406719, lng: -70.742972},
+        zoom: 12
+    });
+}
+
+function comenta(){
+    window.location.href = "index2.html"
+}
+
+function casa(){
+    window.location.href= "index.html"
+}
+
+function calen(){
+    window.location.href= "index3.html"
+}
+
+function conoce(){
+    window.location.href= "index4.html"
+}
